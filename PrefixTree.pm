744222f596dc6161f984de731287fde308fe2073
JosePereiraDev <jpereira.dev@gmail.com>
2015-03-26 18:21:54 +0000

    o rem_word não está a funcionar direito.

--- PrefixTree.pm
+++ PrefixTree.pm
@@ -4,6 +4,7 @@ use Exporter;
 use IO::Uncompress::Gunzip qw(gunzip $GunzipError) ;
 use IO::Uncompress::Bunzip2 qw(bunzip2 $Bunzip2Error);
 use Data::Dumper;
+use Storable;
 our @ISA=qw(Exporter);
 our @EXPORT=qw();
 our @EXPORTER_OK=qw(print save load add_dict add_word rem_word get_words_with_prefix prefix_exists word_exists);
@@ -65,6 +66,98 @@ sub print{
 	my ($self)=@_;
 	my $href=$self->{dicionario};
 	print Dumper($href);
-	delete $href->{$_};
 }
+
+sub save{
+	my ($self,$ficheiro)=@_;
+	store $self,$ficheiro;	
+	}
+	
+sub load{
+	my ($self,$ficheiro)=@_;
+	$self=retrieve($ficheiro);
+	}
+
+sub add_dict{
+	my($self, @ficheiros) = @_;
+	my $dicionario=$self->{dicionario};
+	
+	foreach my $ficheiro (@ficheiros){
+		my ($ext) = $ficheiro =~ /\.([^.]+)$/;
+		if ($ext eq "gz"){
+			print "A descompactar ",$ficheiro,"...\n";
+			my $fh = IO::Uncompress::Gunzip->new($ficheiro) 
+				or die "Erro ao descompactar ",$ficheiro," !!!\n\t-> ",$GunzipError;
+			while(<$fh>) {
+				chomp;
+				$_ =~ s/^\s*(.*?)\s*$/$1/;
+				if($_ ne ""){lepalavra($_,$dicionario);}
+			}
+		}
+		elsif ($ext eq "bz2"){
+			print "A descompactar ",$ficheiro,"...\n";
+			my $fh=new IO::Uncompress::Bunzip2 $ficheiro 
+				or die "Erro ao descompactar ",$ficheiro," !!!\n\t-> ",$Bunzip2Error;
+			while(<$fh>){
+				chomp;
+				$_ =~ s/^\s*(.*?)\s*$/$1/;
+				if($_ ne ""){lepalavra($_,$dicionario);}
+			};
+		}
+		elsif ($ext eq "txt"){
+			my $stream;
+			open($stream, "<", $ficheiro);
+			print "A ler ",$ficheiro," ...\n";
+			while(<$stream>){
+				chomp;
+				if($_ ne ""){lepalavra($_,$dicionario);}
+			};
+		}
+		else {die "Erro: Formato desconhecido em ",$ficheiro,"\n";}
+		
+		}
+	}
+
+sub add_word{
+	my ($self,$palavra)=@_;
+	chomp $palavra;
+	lepalavra($palavra,$self->{dicionario});
+	}
+	
+sub rem_word{
+	my ($self,$palavra)=@_;
+	if($self->word_exists($palavra)==0){die "Palavra nao existe!";}
+	else{
+		my $dic2=$self->{dicionario};
+		my $dic_temp={};
+		my $temp_letra;
+		my $count=0;
+		my @letras=split //, $palavra;
+		foreach my $letra (@letras){
+			$count=values %$dic2;
+			print "COUNT: ",$count,"\n";
+			if($dic2->{$letra}={}){
+				$dic_temp=$dic2->{$letra};
+				$temp_letra=$letra;
+				}
+			delete $dic_temp->{$letra};
+		}
+		$dic_temp={};
+	}
+	}
+
+sub word_exists{
+	my ($self,$palavra)=@_;
+	my $dic2=$self->{dicionario};
+	my @letras=split //, $palavra;
+	foreach my $letra (@letras){
+		if(not exists $dic2->{$letra}){
+			return 0;
+			}
+		$dic2=$dic2->{$letra};
+	}
+	if(!values $dic2) {return 1;}
+	else {return 0;}
+	}
+	
 1;
--- teste.pl
+++ teste.pl
@@ -2,5 +2,38 @@ use strict;
 use warnings;
 use PrefixTree;
 
-my $pt=PrefixTree->new("teste.txt","teste3.gz");
+
+####teste add_dict
+#my $pt=PrefixTree->new("teste.txt");
+#$pt->print();
+#$pt->add_dict("teste3.gz");
+#$pt->print();
+
+
+####teste save /load
+#my $pt=PrefixTree->new("teste.txt");
+#$pt->save("ptree");
+#my $pt2=PrefixTree->load("ptree");
+#$pt2->print();
+
+####teste add_word
+#my $pt=PrefixTree->new("teste.txt");
+#$pt->print();
+#$pt->add_word("lesma");
+#$pt->print();
+
+####teste word_exists
+#my $pt=PrefixTree->new("teste.txt");
+#my $res=$pt->word_exists("ola");
+#if($res==1){print "A palavra ola  existe\n";}
+#elsif($res==0){print "A palavra ola nao existe\n";}
+#my $res2=$pt->word_exists("ol");
+#if($res2==1){print "A palavra ol existe\n";}
+#elsif($res2==0){print "A palavra ol nao existe\n";}
+
+####teste rem_word
+my $pt=PrefixTree->new("teste.txt");
+#$pt->rem_word("ol");
+$pt->print();
+$pt->rem_word("ola");
 $pt->print();
